69365ac634808991c3ec27fe9d62665e7655157d
Thae00 <mathaeoo97@gmail.com>
2022-03-04 12:04:51 +0630

    updating img & container white with stack & align

--- lib/home/food_body_page.dart
+++ lib/home/food_body_page.dart
@@ -8,10 +8,12 @@ class FoodBodyPage extends StatefulWidget {
 }
 
 class _FoodBodyPageState extends State<FoodBodyPage> {
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
+      color: Colors.redAccent,
       child: PageView.builder(
         itemCount: 5,
         itemBuilder: (context, position) {
@@ -22,13 +24,32 @@ class _FoodBodyPageState extends State<FoodBodyPage> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: DecorationImage(
+              image: AssetImage("assets/image/food0.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
