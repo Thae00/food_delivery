eb19dbf8fdac2545e19696f841e64d77e3a771b3
Thae00 <mathaeoo97@gmail.com>
2022-03-29 14:29:49 +0630

    connecting with init,ApiClient,Repository,controllers using getx

---
+++ lib/data/repository/popular_product_repo.dart
@@ -0,0 +1,12 @@
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:get/get.dart';
+
+class PopularProductRepo extends GetxService {
+  final ApiClient apiClient;
+
+  PopularProductRepo({required this.apiClient});
+
+  Future<Response> getPopularPrdouctList() async {
+    return await apiClient.getData("http://mvs.bslmeiyu.com/api/v1/products/popular");
+  }
+}
